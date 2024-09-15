25be82176e89439fb5b99d9d621c74a3accb2b74
rameshteja <ramesh.kreddi@gmail.com>
2024-09-15 18:07:20 +0530

    changes in contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className='text-gray-500'>00000 Willms Station <br/>Suite 000, Washington, USA</p>
-          <p className='text-gray-500'>Tel: (000) 000-0000 <br/> Email: greatstackdev@gmail.com</p>
-          <p className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className='text-gray-500'>Srnagar <br/>Hyderabad, Telangana, INDIA</p>
+          <p className='text-gray-500'>Tel: +91 9848845035 <br/> Email: ramesh.kreddi@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>CAREERS AT Getdoctors</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
